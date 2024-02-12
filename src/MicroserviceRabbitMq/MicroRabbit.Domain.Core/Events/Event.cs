f2f145558cb1aca430e9ca4a03aafb68d5492a55
ctorres014 <ctorres014@gmail.com>
2024-02-12 12:25:16 -0300

    initial commit

---
+++ src/MicroserviceRabbitMq/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime TimesStamp { get; protected set; }
+
+        public Event()
+        {
+            TimesStamp= DateTime.Now;
+        }
+    }
+}
